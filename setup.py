219378f385b0a287ad73379c9ce867e440bdcf47
roman <mster@yandex.ru>
2017-10-17 18:34:15 +0300

    Fix

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='trafficstars',
-    version='1.0',
+    version='1.0.1',
     description='TrafficStars API Client',
     url='https://github.com/Romamo/python-trafficstars',
 
